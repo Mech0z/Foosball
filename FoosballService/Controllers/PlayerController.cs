55bc3161975a913353e5fdde771785d127aee5ff
Skipper <madsskipper@gmail.com>
2018-02-13 18:09:07 +0100

    Migration

---
+++ FoosballService/Controllers/PlayerController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FoosballCore.OldLogic;
+using FoosballCore.RequestResponse;
+using Microsoft.AspNetCore.Mvc;
+using Models.Old;
+using Repository;
+
+namespace FoosballCore.Controllers
+{
+    public class PlayerController : Controller
+    {
+        private readonly IMatchRepository _matchRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IMatchupHistoryCreator _matchupHistoryCreator;
+        private readonly ISeasonLogic _seasonLogic;
+
+        public PlayerController(IMatchRepository matchRepository,
+            IUserRepository userRepository, 
+            IMatchupHistoryCreator matchupHistoryCreator, 
+            ISeasonLogic seasonLogic)
+        {
+            _matchRepository = matchRepository;
+            _userRepository = userRepository;
+            _matchupHistoryCreator = matchupHistoryCreator;
+            _seasonLogic = seasonLogic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Match> GetPlayerMatches(string email)
+        {
+            return _matchRepository.GetPlayerMatches(email).OrderByDescending(x => x.TimeStampUtc);
+        }
+
+        [HttpGet]
+        public List<PartnerPercentResult> GetPlayerPartnerResults(string email)
+        {
+            var activeSeason = _seasonLogic.GetActiveSeason();
+
+            return _matchupHistoryCreator.GetPartnerWinPercent(email, activeSeason.Name);
+        }
+
+        [HttpGet]
+        public IEnumerable<User> GetUsers()
+        {
+            return _userRepository.GetUsers();
+        }
+
+        //[HttpPost]
+        //public IActionResult CreateUser(User user)
+        //{
+        //    var users = _userRepository.GetUsers();
+
+        //    if (string.IsNullOrEmpty(user.Email))
+        //    {
+        //        return BadRequest("Email cant be null");
+        //    }
+
+        //    if (users.Any(x => x.Email == user.Email))
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    _userRepository.AddUser(user);
+        //    return Ok();
+        //}
+
+        //[HttpPost]
+        //public IActionResult Login(User user)
+        //{
+        //    var hash = _userRepository.Login(user);
+        //    if (hash == string.Empty)
+        //    {
+        //        return Unauthorized();
+        //    }
+            
+        //    return Ok(hash);
+        //}
+
+        //[HttpPost]
+        //public IActionResult ChangePassword(ChangePasswordRequest request)
+        //{
+        //    if (request.NewPassword.Length < 6)
+        //    {
+        //        return BadRequest("Password too short");
+        //    }
+
+        //    var hash = _userRepository.ChangePassword(request.Email, request.OldPassword, request.NewPassword);
+            
+        //    if (hash == string.Empty)
+        //    {
+        //        return Unauthorized();
+        //    }
+            
+        //    return Ok(hash);
+        //}
+    }
+}
