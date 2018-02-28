0b60ea6dfccb90ef5c0d3b7e769cf3b07d7819e7
Mads Skipper <madsskipper@gmail.com>
2018-02-28 09:07:20 +0100

    Reenabled start season when logged in

--- FoosballService/Controllers/SeasonsAdministrationController.cs
+++ FoosballService/Controllers/SeasonsAdministrationController.cs
@@ -1,58 +1,35 @@
 ﻿using System.Collections.Generic;
 using FoosballCore.OldLogic;
 using FoosballCore.RequestResponse;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Models.Old;
-using Repository;
 
 namespace FoosballCore.Controllers
 {
     public class SeasonsAdministrationController : Controller
     {
-        private readonly IUserRepository _userRepository;
         private readonly ISeasonLogic _seasonLogic;
 
-        public SeasonsAdministrationController(IUserRepository userRepository, ISeasonLogic seasonLogic)
+        public SeasonsAdministrationController(ISeasonLogic seasonLogic)
         {
-            _userRepository = userRepository;
             _seasonLogic = seasonLogic;
         }
 
-        //[HttpPost]
-        //public IActionResult StartNewSeason(VoidRequest request)
-        //{
-        //    var validated = _userRepository.ValidateAndHasRole(new User
-        //    {
-        //        Email = request.Email,
-        //        Password = request.Password
-        //    }, "Admin");
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public IActionResult StartNewSeason(VoidRequest request)
+        {
+            var seasonName = _seasonLogic.StartNewSeason();
 
-        //    if (!validated)
-        //    {
-        //        return Unauthorized();
-        //    }
-            
-        //    var seasonName = _seasonLogic.StartNewSeason();
+            return Ok(seasonName);
+        }
 
-        //    return Ok(seasonName);
-        //}
-
-        //[HttpPost]
-        //public IActionResult GetSeasons(VoidRequest request)
-        //{
-        //    var validated = _userRepository.ValidateAndHasRole(new User
-        //    {
-        //        Email = request.Email,
-        //        Password = request.Password
-        //    }, "Admin");
-
-        //    if (!validated)
-        //    {
-        //        return Unauthorized();
-        //    }
-
-        //    List<Season> seasons = _seasonLogic.GetSeasons();
-        //    return Ok(seasons);
-        //}
+        [HttpPost]
+        public IActionResult GetSeasons(VoidRequest request)
+        {
+            List<Season> seasons = _seasonLogic.GetSeasons();
+            return Ok(seasons);
+        }
     }
 }
